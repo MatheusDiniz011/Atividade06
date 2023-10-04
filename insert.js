c308436cc328978bd6570c28706e83050f6bbee3
Matheus Diniz <125681876+MatheusDiniz011@users.noreply.github.com>
2023-10-04 14:33:48 -0300

    Add files via upload

---
+++ insert.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql2');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "phpmyadmin",
+  password: "Toji0905",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+
+  console.log("Connected!");
+  var sql = "INSERT INTO customers (name, address) VALUES ('Conpany Inc', 'Highway 37')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+});
+
